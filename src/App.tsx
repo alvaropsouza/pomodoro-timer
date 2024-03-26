e3c68e6808812b260e024f8e26687c7e0e15c655
alvaropsouza <alvaropatricio61@gmail.com>
2024-03-26 10:57:49 -0300

    feat: add default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
